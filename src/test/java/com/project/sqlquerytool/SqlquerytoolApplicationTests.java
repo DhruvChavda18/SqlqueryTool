87b898ba9acb4c23bb15f129a46f26383daf91cf
Dhruv Chavda <dhruvchavda018@gmail.com>
2025-06-26 11:37:51 +0530

    Initial commit

---
+++ src/test/java/com/project/sqlquerytool/SqlquerytoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sqlquerytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlquerytoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
